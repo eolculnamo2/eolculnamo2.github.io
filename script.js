4f20021a8fe6b3ff931d77931a3b5e581302dc58
eolculnamo2 <rbertram8@gmail.com>
2018-03-18 16:12:57 -0500

    start

---
+++ script.js
@@ -0,0 +1,26 @@
+new Vue ({
+    el: "#app",
+    data: {
+        cards: [
+            {
+                title: "Recommended Books",
+                image: "assets/bookapp.png",
+                github: "https://github.com/eolculnamo2/bookapp/",
+                view: "https://afternoon-mesa-31962.herokuapp.com/"
+            },
+            {
+                title: "Wikipedia Vue-er",
+                image: "assets/wiki-vue.png",
+                github: "https://github.com/eolculnamo2/WikipediaViewer/",
+                view: "https://juniper-cormorant.glitch.me/"
+            }
+            ,
+            {
+                title: "Git Profile Viewer",
+                image: "assets/git-profile.png",
+                github: "https://github.com/eolculnamo2/github-profile-viewer",
+                view: "https://future-store.glitch.me/"
+            }
+        ]
+    }
+})
